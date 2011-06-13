3154f46510b3120a322cc239910735c49422a191
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-06-13 06:34:06 +0000

    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/kitPoll/trunk@2 6f821872-b703-e34a-97f4-ccf7fa4d3029

---
+++ info.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * kitPoll
+ * 
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2011
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id$
+ * 
+ * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
+ */
+
+// prevent this file from being accessed directly
+if (!defined('WB_PATH')) die('invalid call of '.$_SERVER['SCRIPT_NAME']);
+
+$module_directory     = 'kit_poll';
+$module_name          = 'kitPoll';
+$module_function      = 'tool';
+$module_version       = '0.10';
+$module_status        = 'Beta';
+$module_platform      = '2.8'; 
+$module_author        = 'Ralf Hertsch, Berlin (Germany)';
+$module_license       = 'GNU General Public License'; 
+$module_description   = 'KIT Poll Extension'; 
+$module_home          = 'http://phpmanufaktur.de/kit_poll';
+$module_guid          = '0D336BD0-AC52-4127-8740-0055C9063991';
+
+?>
