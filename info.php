7bd4fb74684aff2e8a5ba32017434a579da6ccc7
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-06-19 12:22:17 +0200

    Release 0.11
    changed WebsiteBaker version check
    changed license from GNU GPL to MIT license
    added LICENSE
    added CHANGELOG
    removed info.txt
    added README.md
    updated all header informations
    fixed some smaller typos

--- info.php
+++ info.php
@@ -2,29 +2,45 @@
 
 /**
  * kitPoll
- * 
- * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ *
+ * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @link http://phpmanufaktur.de
- * @copyright 2011
- * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
- * @version $Id$
- * 
- * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
+ * @copyright 2011 - 2012
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
 
-// prevent this file from being accessed directly
-if (!defined('WB_PATH')) die('invalid call of '.$_SERVER['SCRIPT_NAME']);
+// include class.secure.php to protect this file and the whole CMS!
+if (defined('WB_PATH')) {
+  if (defined('LEPTON_VERSION'))
+    include(WB_PATH.'/framework/class.secure.php');
+}
+else {
+  $oneback = "../";
+  $root = $oneback;
+  $level = 1;
+  while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+    $root .= $oneback;
+    $level += 1;
+  }
+  if (file_exists($root.'/framework/class.secure.php')) {
+    include($root.'/framework/class.secure.php');
+  }
+  else {
+    trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+  }
+}
+// end include class.secure.php
 
 $module_directory     = 'kit_poll';
 $module_name          = 'kitPoll';
 $module_function      = 'tool';
-$module_version       = '0.10';
+$module_version       = '0.11';
 $module_status        = 'Beta';
-$module_platform      = '2.8'; 
+$module_platform      = '2.8';
 $module_author        = 'Ralf Hertsch, Berlin (Germany)';
-$module_license       = 'GNU General Public License'; 
-$module_description   = 'KIT Poll Extension'; 
-$module_home          = 'http://phpmanufaktur.de/kit_poll';
+$module_license       = 'MIT License (MIT)';
+$module_description   = 'KIT Poll Extension';
+$module_home          = 'http://phpmanufaktur.de';
 $module_guid          = '0D336BD0-AC52-4127-8740-0055C9063991';
 
 ?>
